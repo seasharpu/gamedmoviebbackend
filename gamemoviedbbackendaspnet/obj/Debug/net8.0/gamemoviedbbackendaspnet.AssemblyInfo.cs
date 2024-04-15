6f735fe36b4303cecb9e8bfc56a344be50ba4b78
Seasharpu <zetterstrom.alexander@gmail.com>
2024-04-15 14:28:17 +0200

    added launch.json

--- gamemoviedbbackendaspnet/obj/Debug/net8.0/gamemoviedbbackendaspnet.AssemblyInfo.cs
+++ gamemoviedbbackendaspnet/obj/Debug/net8.0/gamemoviedbbackendaspnet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("gamemoviedbbackendaspnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+869bbcf44d38830b7e0b803143883aa11e2859d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7b6531d9a3b097c1f1667274d67d6c0847e0292")]
 [assembly: System.Reflection.AssemblyProductAttribute("gamemoviedbbackendaspnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("gamemoviedbbackendaspnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
